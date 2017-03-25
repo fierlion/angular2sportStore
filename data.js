b96bc88ea5ba339a0a0e1e8f59f3d1d83b63cb40
fierlion <fierlion@gmail.com>
2017-03-25 15:22:31 -0700

    initial setup with npm and fake middleware

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    return {
+        products: [
+        { id: 1, name: "Kayak", category: "Watersports", description: "A boat for one person", price: 275 },
+        { id: 2, name: "Lifejacket", category: "Watersports", description: "Protective and fashionable", price: 48.95 },
+        { id: 3, name: "Soccer Ball", category: "Soccer", description: "FIFA approved size and weight", price: 19.50 }
+        ],
+        orders: []
+    }
+}
