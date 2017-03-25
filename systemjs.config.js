7ee9b33438ea833d2139d07dcc4256d8a14fdf22
fierlion <fierlion@gmail.com>
2017-03-25 15:36:07 -0700

    configured JavaScript module loader

---
+++ systemjs.config.js
@@ -0,0 +1,14 @@
+(function (global) {
+    var paths = {
+        "@angular/*": "node_modules/@angular/*"
+    }
+    var packages = { "app": {} };
+    var angularModules = ["common", "compiler", "core", "platform-browser-dynamic", "forms", "http", "router"];
+    angularModules.forEach(function (pkg) {
+        packages["@angular/" + pkg] = {
+            main: "/bundles/" + pkg + "umd.min.js"
+        };
+    });
+    System.config({ paths: paths, packages: packages });
+})(this);
+
